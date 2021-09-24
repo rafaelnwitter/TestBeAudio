50940394773c3594a18977fd58f6227f3f22a0a1
Rafael Witt <rafawitt@hotmail.com>
2021-09-24 15:17:14 -0300

    Update files style

--- activies-tracker-frontend/src/App.tsx
+++ activies-tracker-frontend/src/App.tsx
@@ -4,12 +4,36 @@ import { TaskAPI } from './task-api/task.api';
 import { TaskDTO } from './task-api/dto/task.dto';
 import { AppBar, Button, Grid, IconButton, Toolbar, Typography } from '@mui/material';
 import Task from './components/Task';
-// import CreateTaskModal from './components/CreateTaskModal';
+import CreateTaskModal from './components/CreateTaskModal';
+import EditTaskModal from './components/EditTaskModal';
+
 
 function App() {
   const [tasks, setTasks] = useState<TaskDTO[]>([]);
-//  const [creteModalOpen, setCreateModalOpen] = useState(false);
-//  const [createTaskModalOpen, setCreateTaskModalOpen] = useState(false);
+  const [createTaskModalOpen, setCreateTaskModalOpen] = useState(false);
+  const [updateTaskModalOpen, setUpdateTaskModalOpen] = useState(false);
+  const [taskBeingEdited, setTaskBeingEdited] = useState<undefined | TaskDTO>(undefined)
+
+  const addTask = (task: TaskDTO) => {
+    setTasks([...tasks, task])
+  }
+
+  const deleteTask = (taskId: number) => {
+    setTasks(tasks.filter((x) => x.id !== taskId));
+  }
+
+  const updateTask = (task: TaskDTO) => {
+    setTasks(
+      tasks.map(x => {
+        if(x.id === task.id) return task;
+        return x;
+      })
+    );
+  }
+
+  const onTaskEdotBtnClicked = (task: TaskDTO) => {
+
+  }
 
   useEffect(() => {
     async function fetchAll() {
@@ -21,16 +45,28 @@ function App() {
     fetchAll();
   }, [])
 
-// <CreateTaskModal open={createTaskModalOpen} handleClose={() => setCreateTaskModalOpen(false)} />
-
   return (
     <div className="App">
+      <CreateTaskModal 
+        open={createTaskModalOpen} 
+        handleClose={() => setCreateTaskModalOpen(false)} 
+        onTaskCreated={addTask}
+      />
+      <EditTaskModal 
+        data={taskBeingEdited}
+        open={updateTaskModalOpen} 
+        handleClose={() => setUpdateTaskModalOpen(false)} 
+        onTaskUpdate={updateTask}
+      />
       <AppBar position="static">
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Dashboard Tasks
           </Typography>
-          <Button variant="contained" color="primary">Create Task</Button>
+          <Button
+            variant="contained" 
+            color="primary" 
+            onClick={() => setCreateTaskModalOpen(true)}>Create Task</Button>
         </Toolbar>
       </AppBar>
       
@@ -38,7 +74,10 @@ function App() {
       {tasks.map(task => {
           return (
           <Grid item xs={3} key={task.id}>
-            <Task data={task} />
+            <Task data={task} onTaskDelete={deleteTask} onTaskUpdate={(task: TaskDTO) => {
+              setTaskBeingEdited(task);
+              setUpdateTaskModalOpen(true);
+            }}/>
           </Grid>);
          })}
       </Grid>
--- activies-tracker-frontend/src/components/Task.tsx
+++ activies-tracker-frontend/src/components/Task.tsx
@@ -1,17 +1,44 @@
 import React from 'react';
-import { TaskDTO } from "../task-api/dto/task.dto";
+import { TaskDTO, TaskStatus } from "../task-api/dto/task.dto";
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import { Container } from '@mui/material';
+import { Chip, Container } from '@mui/material';
+import { TaskAPI } from '../task-api/task.api';
 
 interface Props {
-    data: TaskDTO;   
+    data: TaskDTO;
+    onTaskDelete: (taskId: number) => void;
+    onTaskUpdate: (task: TaskDTO) => void;
+
 }
 
-const Task = ({ data }: Props) => {
+const Task = ({ data, onTaskDelete, onTaskUpdate }: Props) => {
+    const deleteTask = async () => {
+        await TaskAPI.deleteOne(data.id);
+        // onTaskDelete(data.id);
+    };
+
+    const getTaskStatusToString = (status: TaskStatus) => {
+        let text: string;
+
+        switch(status){
+            case TaskStatus.Created:
+                text = "Created";
+                break;
+            case TaskStatus.InProgress:
+                text = "In Progress";
+                break;
+            case TaskStatus.Done:
+                text = "Done";
+                break;
+            default:
+                text = "";
+        }
+        return text
+    }
     return (
         <Card variant="outlined">
             <CardContent>
@@ -25,19 +52,27 @@ const Task = ({ data }: Props) => {
                   variant="body2" component="p">
                     {data.description}
                 </Typography>
-                <Typography
-                  variant="body2" component="p">
-                    {data.type}
-                </Typography>
-                <Typography
-                  variant="body2" component="p">
-                    {data.status}
-                </Typography>
+                <Chip label={getTaskStatusToString(data.status)} />
             </CardContent>
             <CardActions>
                 <Container>
-                    <Button style={{ marginLeft: 5 }}size="small" variant="contained" color="primary">Edit</Button>
-                    <Button style={{ marginLeft: 5 }}size="small" variant="contained" color="secondary">Delete</Button>
+                    <Button 
+                        style={{ marginLeft: 5 }}
+                        size="small" 
+                        variant="contained"
+                        color="primary"
+                        onClick={() => onTaskUpdate(data)}
+                        >
+                            Edit
+                    </Button>
+                    <Button 
+                        style={{ marginLeft: 5 }}
+                        size="small" 
+                        variant="contained" 
+                        color="secondary"
+                        onClick={deleteTask}>
+                            Delete
+                    </Button>
                 </Container>
             </CardActions>
         </Card>
